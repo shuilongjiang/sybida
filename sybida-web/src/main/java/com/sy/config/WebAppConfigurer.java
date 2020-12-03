536f3e32517319e27339cb2a891a06a5eafe9fc2
shuilongjiang <shuilongjiang@126.com>
2020-12-03 17:59:17 +0800

    添加过滤器

---
+++ sybida-web/src/main/java/com/sy/config/WebAppConfigurer.java
@@ -0,0 +1,26 @@
+package com.sy.config;
+
+import com.sy.inteceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+    //    @Autowired
+//    LoginInterceptor loginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns(urlPass());
+    }
+
+    public List<String> urlPass(){
+        List<String> list = new ArrayList<>();
+        list.add("/login");
+        // 。。。。
+        return list;
+    }
+}
---
+++ sybida-web/src/main/java/com/sy/inteceptor/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.sy.inteceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("============preHandle========");
+//
+//        HttpSession session = request.getSession();
+//        Object object = session.getAttribute("user");
+//        if(null != object) {
+//            return true;
+//        } else{
+//            response.sendRedirect("http://localhost:63343/mvboot/home.html");
+//            return false;
+//        }
+        return true;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
