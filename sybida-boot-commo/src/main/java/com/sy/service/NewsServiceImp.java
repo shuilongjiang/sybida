c81fd78615c0c129139363ca7c342a931be8eca7
shuilongjiang <shuilongjiang@126.com>
2020-12-18 15:31:23 +0800

    jsl保存

---
+++ sybida-boot-commo/src/main/java/com/sy/service/NewsServiceImp.java
@@ -0,0 +1,33 @@
+package com.sy.service;
+
+import com.sy.mapper.SybidaNewsMapper;
+import com.sy.pojo.SybidaNews;
+import com.sy.pojo.SybidaNewsExample;
+import com.sy.vo.ResponseResult;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class NewsServiceImp implements NewsService{
+    @Autowired
+    SybidaNewsMapper sybidaNewsMapper;
+    @Transactional
+    @Override
+    public ResponseResult hadSendMessage(String userid) {
+        SybidaNewsExample sybidaNewsExample=new SybidaNewsExample();
+        sybidaNewsExample.createCriteria().andNewsUserIdEqualTo(Integer.valueOf(userid));
+        List<SybidaNews> list = sybidaNewsMapper.selectByExample(sybidaNewsExample);
+        ResponseResult responseResult = new ResponseResult();
+        if(null!=list && list.size()>0){
+            responseResult.setCode(list.size());
+        }else{
+            responseResult.setCode(0);
+        }
+        responseResult.setData(list);
+        return responseResult;
+    }
+}
