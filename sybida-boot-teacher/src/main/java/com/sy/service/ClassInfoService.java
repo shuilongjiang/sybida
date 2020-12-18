2bdf1d3bf664df7f388e9e18e05c73f4cb16abd7
sybida <422434393@qq.com>
2020-12-18 18:05:20 +0800

    备份一下

--- sybida-boot-teacher/src/main/java/com/sy/service/ClassInfoService.java
+++ sybida-boot-teacher/src/main/java/com/sy/service/ClassInfoService.java
@@ -2,6 +2,7 @@ package com.sy.service;
 
 import com.sy.pojo.SybidaClass;
 import com.sy.pojo.SybidaStudy;
+import com.sy.pojo.SybidaTeach;
 import com.sy.vo.ResponseResult;
 
 import java.util.List;
@@ -22,4 +23,8 @@ public interface ClassInfoService {
     ResponseResult deleteAllClass(List<Integer> list);
 
     SybidaClass showOneClass(String classId);
+
+    SybidaTeach selectTeacherName(String classTeachId);
+
+    SybidaStudy selectStudyName(String classStudyId);
 }
