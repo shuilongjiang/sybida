fd470aa5758a47f14ceb87c34d9462628f0c58ff
shuilongjiang <shuilongjiang@126.com>
2020-12-23 19:44:29 +0800

    文件压缩下载

--- sybida-boot-commo/src/main/java/com/sy/service/ScheduledService.java
+++ sybida-boot-commo/src/main/java/com/sy/service/ScheduledService.java
@@ -27,7 +27,7 @@ public class ScheduledService {
 //    }
     @Autowired
     RedisUtil redisUtil;
-    @Scheduled(fixedDelay = 1000*60)
+    @Scheduled(fixedDelay = 1000*60*10)
     public void scheduled2() {
         System.out.println("定时删除开启"+ System.currentTimeMillis());
         DeleteFileAndPackage.deleteDir("E:\\upload\\zip");
