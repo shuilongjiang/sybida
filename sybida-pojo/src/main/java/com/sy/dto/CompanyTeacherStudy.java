9f625136592f90ef1e10247a15f3cf7918e39e78
RainGYY <1277452848@qq.com>
2020-12-25 08:52:55 +0800

    company实体类

---
+++ sybida-pojo/src/main/java/com/sy/dto/CompanyTeacherStudy.java
@@ -0,0 +1,225 @@
+package com.sy.dto;
+
+import java.util.Date;
+
+public class CompanyTeacherStudy {
+    private Integer companyId;
+
+    private Integer companyUserId;
+
+    private Integer companyMarkId;
+
+    private String companyName;
+
+    private Date companyEndTime;
+
+    private Date companyStartTime;
+
+    private Integer companyStudyId;
+
+    private String companyWeb;
+
+    private String companyAddress;
+
+    private String companyRequire;
+
+    private Integer companySalary;
+
+    private String companyMailbox;
+
+    private String companyWechat;
+
+    private String companyPhone;
+
+    private String companyIntroduce;
+
+    private String companyPicture;
+
+    private Byte companyIsView;
+
+    private Date companyAlterTime;
+
+    private String companyNull1;
+
+    private String companyNull2;
+
+    private String studyAspect;
+
+    private String teachName;
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getCompanyUserId() {
+        return companyUserId;
+    }
+
+    public void setCompanyUserId(Integer companyUserId) {
+        this.companyUserId = companyUserId;
+    }
+
+    public Integer getCompanyMarkId() {
+        return companyMarkId;
+    }
+
+    public void setCompanyMarkId(Integer companyMarkId) {
+        this.companyMarkId = companyMarkId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Date getCompanyEndTime() {
+        return companyEndTime;
+    }
+
+    public void setCompanyEndTime(Date companyEndTime) {
+        this.companyEndTime = companyEndTime;
+    }
+
+    public Date getCompanyStartTime() {
+        return companyStartTime;
+    }
+
+    public void setCompanyStartTime(Date companyStartTime) {
+        this.companyStartTime = companyStartTime;
+    }
+
+    public Integer getCompanyStudyId() {
+        return companyStudyId;
+    }
+
+    public void setCompanyStudyId(Integer companyStudyId) {
+        this.companyStudyId = companyStudyId;
+    }
+
+    public String getCompanyWeb() {
+        return companyWeb;
+    }
+
+    public void setCompanyWeb(String companyWeb) {
+        this.companyWeb = companyWeb;
+    }
+
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public String getCompanyRequire() {
+        return companyRequire;
+    }
+
+    public void setCompanyRequire(String companyRequire) {
+        this.companyRequire = companyRequire;
+    }
+
+    public Integer getCompanySalary() {
+        return companySalary;
+    }
+
+    public void setCompanySalary(Integer companySalary) {
+        this.companySalary = companySalary;
+    }
+
+    public String getCompanyMailbox() {
+        return companyMailbox;
+    }
+
+    public void setCompanyMailbox(String companyMailbox) {
+        this.companyMailbox = companyMailbox;
+    }
+
+    public String getCompanyWechat() {
+        return companyWechat;
+    }
+
+    public void setCompanyWechat(String companyWechat) {
+        this.companyWechat = companyWechat;
+    }
+
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
+
+    public void setCompanyPhone(String companyPhone) {
+        this.companyPhone = companyPhone;
+    }
+
+    public String getCompanyIntroduce() {
+        return companyIntroduce;
+    }
+
+    public void setCompanyIntroduce(String companyIntroduce) {
+        this.companyIntroduce = companyIntroduce;
+    }
+
+    public String getCompanyPicture() {
+        return companyPicture;
+    }
+
+    public void setCompanyPicture(String companyPicture) {
+        this.companyPicture = companyPicture;
+    }
+
+    public Byte getCompanyIsView() {
+        return companyIsView;
+    }
+
+    public void setCompanyIsView(Byte companyIsView) {
+        this.companyIsView = companyIsView;
+    }
+
+    public Date getCompanyAlterTime() {
+        return companyAlterTime;
+    }
+
+    public void setCompanyAlterTime(Date companyAlterTime) {
+        this.companyAlterTime = companyAlterTime;
+    }
+
+    public String getCompanyNull1() {
+        return companyNull1;
+    }
+
+    public void setCompanyNull1(String companyNull1) {
+        this.companyNull1 = companyNull1;
+    }
+
+    public String getCompanyNull2() {
+        return companyNull2;
+    }
+
+    public void setCompanyNull2(String companyNull2) {
+        this.companyNull2 = companyNull2;
+    }
+
+    public String getStudyAspect() {
+        return studyAspect;
+    }
+
+    public void setStudyAspect(String studyAspect) {
+        this.studyAspect = studyAspect;
+    }
+
+    public String getTeachName() {
+        return teachName;
+    }
+
+    public void setTeachName(String teachName) {
+        this.teachName = teachName;
+    }
+}
